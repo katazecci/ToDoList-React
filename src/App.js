54ea5bc0014e843bb587013d9133289b226e938e
katazecci <kataaz@hotmail.com>
2023-02-15 17:04:04 +0200

    Added small functionalities

--- src/App.js
+++ src/App.js
@@ -2,31 +2,46 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const [desc, setDesc] = React.useState('');
+  const [todo, setTodo] = React.useState({description: '', date: ''});
   const [todos, setTodos] = React.useState([]);
 
   const inputChanged = (event) => {
-    setDesc(event.target.value);
+    const {name, value} = event.target;
+    setTodo({...todo, [name]: value});
   }
 
   const addTodo = (event) => {
     event.preventDefault();
-    setTodos([...todos, desc]);
+    setTodos([...todos, todo]);
+    setTodo({description: '', date: ''});
   }
-
   return (
     <div>
-      <input type="text" onChange={inputChanged} value={desc}/>
-      <button onClick={addTodo}>Add</button>
       <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Date</th>
+          </tr>
+        </thead>
         <tbody>
+          <tr>
+            <td>
+              <input type="text" name="description" onChange={inputChanged} value={todo.description} placeholder="Description" />
+            </td>
+            <td>
+              <input type="date" name="date" onChange={inputChanged} value={todo.date} placeholder="Date" />
+            </td>
+            <td>
+              <button onClick={addTodo}>Add</button>
+            </td>
+          </tr>
           {
-            todos.map(todo => <tr><td>{todo}</td></tr>)
+            todos.map((todo, index) => <tr key={index}><td>{todo.description}</td><td>{todo.date}</td></tr>)
           }
         </tbody>
-      </table>   
+      </table>
     </div>
   );
-};
-
+}
 export default App;
