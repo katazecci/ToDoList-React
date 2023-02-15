c96bd9c17088d6a4c6d196827c2bb10161d5f36a
katazecci <kataaz@hotmail.com>
2023-02-15 16:06:14 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [desc, setDesc] = React.useState('');
+  const [todos, setTodos] = React.useState([]);
+
+  const inputChanged = (event) => {
+    setDesc(event.target.value);
+  }
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, desc]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={inputChanged} value={desc}/>
+      <button onClick={addTodo}>Add</button>
+      <table>
+        <tbody>
+          {
+            todos.map(todo => <tr><td>{todo}</td></tr>)
+          }
+        </tbody>
+      </table>   
     </div>
   );
-}
+};
 
 export default App;
